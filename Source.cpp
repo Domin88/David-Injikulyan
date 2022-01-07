221d293872bac77fc95ca85cdb084a33663f6b81
Domin88 <97161776+Domin88@users.noreply.github.com>
2022-01-07 16:20:26 +0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	int n;
+	float a = 1000;
+	cout << "nermuceq metry  ";
+	cin >> n;
+	cout << n << "  metry klini  " << n / a<< "  kilometr";
+	return 0;
+
+
+
+}
